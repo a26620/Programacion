8829990edc5f4753fbe694c2fbc98b3536f1654d
David Mateo Leal <130655723+a26620@users.noreply.github.com>
2023-10-17 10:48:54 +0200

    .

--- Android Studio/Login_03102023/app/src/main/java/com/example/login_03102023/MainActivity.java
+++ Android Studio/Login_03102023/app/src/main/java/com/example/login_03102023/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.login_03102023;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.example.login_03102023.actions.ServicePeliculas;
+import com.example.login_03102023.actions.ViewPeliculas;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements ViewPeliculas {
 
     private EditText edtEmail;
     private EditText edtPassword;
     private Button btnLogin;
+
+    private ServicePeliculas sPeliculas = new ServicePeliculas(this);
+
+    /*PATRON SINGLETON*/
+    private static MainActivity mainActivity = null;
+
+    public static MainActivity getInstance(){
+        return mainActivity;
+    }
+    /*FIN PATRON SINGLETON*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +41,12 @@ public class MainActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.edtEmail);
         edtPassword = findViewById(R.id.edtPassword);
         btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
     }
 
 
+    @Override
+    public void showPeliculas(String message) {
 
+    }
 }
---
+++ Android Studio/Login_03102023/app/src/main/java/com/example/login_03102023/actions/ViewPeliculas.java
@@ -0,0 +1,5 @@
+package com.example.login_03102023.actions;
+
+public interface ViewPeliculas {
+    void showPeliculas(String message);
+}
