2f46830d08a5712074731cf34aadba7058f473d0
David Mateo Leal <130655723+a26620@users.noreply.github.com>
2023-11-20 12:55:38 +0100

    .

---
+++ App Para Diciembre/LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.loginandroid_29_09_2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.loginandroid_29_09_2023.beans.User;
+import com.example.loginandroid_29_09_2023.login_user.ContractLoginUser;
+import com.example.loginandroid_29_09_2023.login_user.presenter.LoginUserPresenter;
+import com.example.loginandroid_29_09_2023.login_user.view.LoginUserM;
+import com.example.loginandroid_29_09_2023.lstMov.view.LstMovies;
+import com.example.loginandroid_29_09_2023.lst_movies_2.view.LstPelicula2Activity;
+
+public class MainActivity extends AppCompatActivity{
+    private static final long SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.SplashScreen);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(MainActivity.this,
+                        LoginUserM.class);
+                startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
